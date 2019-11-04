aa83473cc53afd8d904b5b54971f65921caaa143
Krylova Maria <krylova.maria.2629@gmail.com>
2019-11-04 22:18:41 +0300

    solutions v.1

---
+++ include/task1.h
@@ -0,0 +1,9 @@
+//
+// Created by Mary on 04.11.2019.
+//
+
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+double calcDelta();
+double calcCost();
+#endif //TASK1_TASK1_H
---
+++ include/task2.h
@@ -0,0 +1,8 @@
+//
+// Created by Mary on 04.11.2019.
+//
+
+#ifndef TASK1_TASK2_H
+#define TASK1_TASK2_H
+
+#endif //TASK1_TASK2_H
