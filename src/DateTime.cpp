aaf796297c95bba0ce239a18e14c72d89519561e
Andrei Zhuravlev <dwasdx@gmail.com>
2019-11-06 20:56:03 +0300

    ...(3)

--- src/DateTime.cpp
+++ src/DateTime.cpp
@@ -9,7 +9,6 @@
 #include "DateTime.h"
 #include <vector>
 #include <cstring>
-#include <string.h>
 
 //CHECK FOR SPELLING
 std::vector<std::string> months = {
@@ -49,9 +48,9 @@ tm* DateTime::_copyDate(tm* src) {
     dest->tm_yday = src->tm_yday;
     dest->tm_isdst = src->tm_isdst;
     dest->tm_gmtoff = src->tm_gmtoff;
-//    dest->tm_zone = new char[strlen(src->tm_zone)];
-//    const char* src_str = src->tm_zone;
-//    strcpy(dest->tm_zone, src_str);
+    dest->tm_zone = new char[strlen(src->tm_zone)];
+    const char* src_str = src->tm_zone;
+    strcpy(dest->tm_zone, src_str);
     return dest;
 }
 
@@ -125,10 +124,7 @@ std::string DateTime::getPast(unsigned int diff) {
 unsigned int DateTime::getDifference(DateTime &date) {
     time_t diff = difftime(date._time_value, _time_value);
     diff = abs(diff);
-    tm *temp = localtime(&diff);
-    unsigned int res = temp->tm_yday - 1;
-    localtime(&_time_value);
-    return res;
+    return diff / 86400;
 }
 
 
