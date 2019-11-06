eda5b526c47eba0c5f30777964b9a8c208e7d9db
Andrei Zhuravlev <dwasdx@gmail.com>
2019-11-06 19:03:08 +0300

    hotfix

--- src/main1.cpp
+++ src/main1.cpp
@@ -11,6 +11,6 @@
 
 
 
-//int main() {
-//    std::cout << calcDelta() << std::endl;
-//}
+int main() {
+    std::cout << calcDelta() << std::endl;
+}
--- src/main2.cpp
+++ src/main2.cpp
@@ -11,6 +11,6 @@
 
 
 
-//int main() {
-//    std::cout << calcCost() << std::endl;
-//}
+int main() {
+    std::cout << calcCost() << std::endl;
+}
