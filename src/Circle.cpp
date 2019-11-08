5ee2f831a4ed570e7fe14ee944a6204d8cee4b0b
smvolokh <sema.volokh@yandex.ru>
2019-11-08 23:07:26 +0300

    M_PI -> pi

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,27 +1,28 @@
 #pragma once
 #include "Circle.h"
-#define M_PI 3.1415
+#define pi 3.1415
 #include <math.h>
+
 Circle::Circle(double radius){
 	Circle::setRadius(radius);
 }
 
 void Circle::setRadius(double radius){
 	this->radius = radius;
-	ference = 2 * M_PI * radius;
-	area = M_PI * radius * radius;
+	ference = 2 * pi * radius;
+	area = pi * radius * radius;
 }
 
 void Circle::setFerence(double ference){
 	this->ference = ference;
-	radius = ference / (2 * M_PI);
-	area = M_PI * radius * radius;
+	radius = ference / (2 * pi);
+	area = pi * radius * radius;
 }
 
 void Circle::setArea(double area){
 	this->area = area;
-	radius = sqrt(area / M_PI);
-	ference = 2 * M_PI * radius;
+	radius = sqrt(area / pi);
+	ference = 2 * pi * radius;
 }
 
 double Circle::getRadius(){
