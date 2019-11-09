bc7bc7fa8418fe8cb4854069ba7239c26c14e985
Polina Demochkina <lollipoli@live.com>
2019-11-09 15:30:24 +0300

    Fixed a syntax mistake

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "task1.h";
+#include "task1.h"
 
 Circle::Circle(double value)
 {
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,5 +1,5 @@
 #include "task1.h"
-#include "circle.h"
+#include "Circle.h"
 
 double calcDelta()
 {
