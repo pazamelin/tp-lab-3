4d02f5a419c2f9c58efe2efd5ae96082188be51c
mp913 <zarkov01@mail.ru>
2019-11-11 19:15:10 +0300

    Changed pi number

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,30 +1,30 @@
 #include <math.h>
-#define pi 3.14159265359
+#include <cmath>
 #include "Circle.h"
 
 Circle::Circle(double r)
 {
 	radius = r;
-	ference = 2 * pi * r;
-	area = pi * r * r;
+	ference = 2 * M_PI * r;
+	area = M_M_PI * r * r;
 }
 void Circle::setRadius(double r)
 {
 	radius = r;
-	ference = 2 * pi * r;
-	area = pi * r*r;
+	ference = 2 * M_PI * r;
+	area = M_PI * r * r;
 }
 void Circle::setFerence(double f)
 {
 	ference = f;
-	radius = f / (2 * pi);
-	area = pi * radius * radius;
+	radius = f / (2 * M_PI);
+	area = M_PI * radius * radius;
 }
 void Circle::setArea(double a)
 {
 	area = a;
-	radius = sqrt(a / pi);
-	ference = 2 * pi * radius;
+	radius = sqrt(a / M_PI);
+	ference = 2 * M_PI * radius;
 }
 double Circle::getRadius()
 {
