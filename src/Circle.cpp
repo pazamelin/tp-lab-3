2961f457fb6f16bcb4cdeaac614228bca2eb7185
mp913 <zarkov01@mail.ru>
2019-11-11 19:29:41 +0300

    New order in formula

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -17,7 +17,7 @@ void Circle::setRadius(double r)
 void Circle::setFerence(double f)
 {
 	ference = f;
-	radius = f / (2 * M_PI);
+	radius = f / M_PI / 2;
 	area = M_PI * radius * radius;
 }
 void Circle::setArea(double a)
