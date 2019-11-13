fd49b9cfa2bc0a6b6c6e7a628f45cf9aa9bdceec
Doroshenko Ilya <45979573+iddoroshenko@users.noreply.github.com>
2019-11-13 14:32:21 +0300

    const methods again

--- include/DateTime.h
+++ include/DateTime.h
@@ -5,14 +5,14 @@ public:
 	DateTime(int, int, int);
 	DateTime();
 	DateTime(DateTime&);	
-	std::string getToday();
-	std::string getYesterday(); 
-	std::string getTomorrow();
-	std::string getFuture(unsigned int);
-	std::string getPast(unsigned int);
-	int getDifference(DateTime&);
+	std::string getToday() const;
+	std::string getYesterday() const; 
+	std::string getTomorrow() const;
+	std::string getFuture(unsigned int) const;
+	std::string getPast(unsigned int) const;
+	int getDifference(DateTime&) const;
 private:
-	std::string getData(int);
+	std::string getData(int) const;
 	int day;
 	int month;
 	int year;
