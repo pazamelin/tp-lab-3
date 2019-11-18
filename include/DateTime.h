b76d6decb4e9d3c7ec15282259e472a126cb84be
mp913 <zarkov01@mail.ru>
2019-11-18 17:55:38 +0300

    Uodated DateTime

--- include/DateTime.h
+++ include/DateTime.h
@@ -1,19 +1,23 @@
 #include <ctime>
 #include <string>
+#include <vector>
 
 #ifndef DATETIME_H
 #define DATETIME_H
 class DateTime {
-	struct tm* dt;
+	struct tm dt;
+	static std::string week_days[];
+	static std::string months[12];
 public:
+	//~DateTime();
 	DateTime(int day, int month, int year);
 	DateTime();
 	DateTime(const DateTime &origin);
-	std::string getToday(); // возвращение текущей даты в виде строки, с указанием дня недели и названия месяца(например 07 november 2018, wednesday)
-	std::string getYesterday(); // -возвращение даты вчерашнего дня в виде строк.
-	std::string getTomorrow(); // -возвращение даты завтрашнего дня в виде строки
-	std::string getFuture(unsigned int n); // -возвращение даты через N дней в будущем
-	std::string getPast(unsigned int n); // - возвращение даты через N дней в прошлом
+	std::string getToday()const; // возвращение текущей даты в виде строки, с указанием дня недели и названия месяца(например 07 november 2018, wednesday)
+	std::string getYesterday()const; // -возвращение даты вчерашнего дня в виде строк.
+	std::string getTomorrow()const; // -возвращение даты завтрашнего дня в виде строки
+	std::string getFuture(unsigned int n)const; // -возвращение даты через N дней в будущем
+	std::string getPast(unsigned int n)const; // - возвращение даты через N дней в прошлом
 	int getDifference(DateTime& target); // - для расчёта разницы(в днях) между двумя датами
 };
 #endif
--- src/DateTime.cpp
+++ src/DateTime.cpp
@@ -5,60 +5,65 @@
 #include <ctime>
 #include <vector>
 
+std::string DateTime::week_days[] = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
+std::string DateTime::months[] = { "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" };;
+
+//DateTime::~DateTime()
+//{
+//	delete dt;
+//}
+
 DateTime::DateTime(int d, int m, int y)
 {
-	time_t tmp = time(nullptr);
-	dt = new tm;
-	memcpy(dt, localtime(&tmp), sizeof(tm));
-	dt->tm_mday = d;
-	dt->tm_mon = m - 1;
-	dt->tm_year = y - 1900;
-	mktime(dt);
+	dt.tm_mday = d;
+	dt.tm_mon = m - 1;
+	dt.tm_year = y - 1900;
+	dt.tm_sec = 0;
+	dt.tm_min = 0;
+	dt.tm_hour = 0;
+	mktime(&dt);
 }
 
 DateTime::DateTime()
 {
 	time_t tmp = time(nullptr);
-	dt = localtime(&tmp);
+	dt = *(localtime(&tmp));
 }
 
 DateTime::DateTime(const DateTime &origin)
 {
-	dt = new tm;
-	memcpy(dt, origin.dt, sizeof(tm));
+	memcpy(&dt, &(origin.dt), sizeof(tm));
 }
 
-std::string DateTime::getToday()
+std::string DateTime::getToday() const
 {
-	std::vector<std::string> week_days = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
-	std::vector<std::string> months= { "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" };
 	std::string day = "";
-	if (dt->tm_mday < 10) day += "0";
-	day += std::to_string(dt->tm_mday);
-	return (day + ' ' +	months[dt->tm_mon] + ' ' + std::to_string(dt->tm_year + 1900) + ", " + week_days[dt->tm_wday]);
+	if (dt.tm_mday < 10) day += "0";
+	day += std::to_string(dt.tm_mday);
+	return (day + ' ' + months[dt.tm_mon] + ' ' + std::to_string(dt.tm_year + 1900) + ", " + week_days[dt.tm_wday]);
 }
 
-std::string DateTime::getYesterday()
+std::string DateTime::getYesterday() const
 {
 	return this->getPast(1);
 }
 
-std::string DateTime::getTomorrow()
+std::string DateTime::getTomorrow() const
 {
 	return this->getFuture(1);
 }
 
-std::string DateTime::getFuture(unsigned int n)
+std::string DateTime::getFuture(unsigned int n) const
 {
-	return DateTime(dt->tm_mday + n, dt->tm_mon + 1, dt->tm_year + 1900).getToday();
+	return DateTime(dt.tm_mday + n, dt.tm_mon + 1, dt.tm_year + 1900).getToday();
 }
 
-std::string DateTime::getPast(unsigned int n)
+std::string DateTime::getPast(unsigned int n) const
 {
-	return DateTime(dt->tm_mday - n, dt->tm_mon + 1, dt->tm_year + 1900).getToday();
+	return DateTime(dt.tm_mday - n, dt.tm_mon + 1, dt.tm_year + 1900).getToday();
 }
 
-int DateTime::getDifference(DateTime& target)
+int DateTime::getDifference(DateTime& target) 
 {
-	return abs((mktime(this->dt) - mktime(target.dt))) / 86400; // количество секунд в сутках
+	return abs(mktime(&(this->dt)) - mktime(&(target.dt))) / 86400; // количество секунд в сутках
 }
