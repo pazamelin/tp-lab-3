94f0d11475aa357915f5ca25e1341c447b789efe
Sasha Vokhmyanina <SashaVokhmyanina@yandex.ru>
2019-11-19 16:03:58 +0300

    test

--- test/tests.cpp
+++ test/tests.cpp
@@ -59,7 +59,7 @@ TEST(date,datetime_getdiff2)
 }
 TEST(calc,delta)
 {    
-    EXPECT_DOUBLE_EQ(0.15916,calcDelta());
+	EXPECT_DOUBLE_EQ(0.15915963705629110, calcDelta());
 }
 TEST(calc,pool_cost)
 {    
