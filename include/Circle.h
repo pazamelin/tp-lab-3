1e05eb6b3cae081a39f760f049f272d509900eda
kozlova_oksana <kozlova.ksucha@yandex.ru>
2019-12-08 22:15:54 +0300

    src

---
+++ include/Circle.h
@@ -0,0 +1,8 @@
+//
+// Created by Оксана on 18.11.2019.
+//
+
+#ifndef TASK1_CIRCLE_H
+#define TASK1_CIRCLE_H
+
+#endif //TASK1_CIRCLE_H
---
+++ include/DateTime.h
@@ -0,0 +1,8 @@
+//
+// Created by Оксана on 18.11.2019.
+//
+
+#ifndef TASK1_DATETIME_H
+#define TASK1_DATETIME_H
+
+#endif //TASK1_DATETIME_H
