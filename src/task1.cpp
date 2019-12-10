d392ff0b6f80a0d8d248aad1100fc9fdbe0e67dd
Valerii <valerider@icloud.com>
2019-12-10 19:04:15 +0300

    task1.cpp fixed

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,14 +1,13 @@
 #include "Circle.h"
-#include <iostream>
 void calcDelta() {
 	double rad = 6378.1;
 	Circle planet(rad), newPlanet(0);
 	newPlanet.setFerence(planet.getFerence() + 0.1);
-	std::cout << newPlanet.getRadius() - planet.getRadius() << std::endl;
+	return newPlanet.getRadius() - planet.getRadius();
 }
 void calcCost() {
 	Circle pool(3), road(4);
 	double cost = (road.getArea() - pool.getArea()) * 1000;
 	cost += road.getFerence() * 2000;
-	std::cout << cost << std::endl;
+	return cost;
 }
