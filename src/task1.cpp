7a1bc839ca7de2e1bd0d92946e4aacc26c9ae193
kozlova_oksana <kozlova.ksucha@yandex.ru>
2019-12-18 11:56:18 +0300

    Kozlova Oksana

--- src/task1.cpp
+++ src/task1.cpp
@@ -12,22 +12,16 @@ double calcDelta(){
 }
 
 double calcCost() {
-    /*double concrete_cost = 1000.0;
+    double concrete_cost = 1000.0;
     double ference_cost = 2000.0;
     double pool_radius = 3.0;
     double track_width = 1.0;
     Circle pool(pool_radius);
     double pool_area = pool.getArea();
     pool.setRadius(pool_radius + track_width);
-    return (pool.getArea() - pool_area) * concrete_cost + pool.getFerence() * ference_cost; */
+    return (pool.getArea() - pool_area) * concrete_cost + pool.getFerence() * ference_cost;
 
-    double concreteCostPerSquareMeter = 1000;  // in Russian rubles
-    double  fenceCostPerMeter = 2000;
-    double radius = 3;
-    Circle pool = Circle(radius);
-    double poolArea = pool.getArea();
-    pool.setRadius(radius + 1);
-    return fenceCostPerMeter * pool.getFerence() + (pool.getArea() - poolArea) * concreteCostPerSquareMeter;
+
 
 
 
