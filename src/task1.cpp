d26c31fb6b75728c04a8e9798bed44c43f2ae04c
Олеся Мартынюк <olesyamartinyuk@gmail.com>
2019-12-18 08:38:27 +0300

    dumb fix 2

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,14 +1,14 @@
 #include "Circle.h"
 #include "task1.h"
 
-double land_and_rope(){
+double calcDelta(){
     Circle Earth(6378.1);
     Circle Rope(0);
     Rope.setFerence(Earth.getFerence()+1);
     return double(Rope.getRadius() - Earth.getRadius());
 }
 
-double swimming_pool(){
+double calcCost(){
     Circle Pool(3);
     Circle Road(Pool.getRadius()+1);
     return double(1000*(Road.getArea() - Pool.getArea()) + 2000*Road.getFerence());
